0b598fbce098b2160f3acedb0a945244ae6b2c68
AyalaOnGit <arh67637@gmail.com>
2026-02-19 19:51:11 +0200

    7 update of shop-frontend

--- src/app/components/products-page/products-page.ts
+++ src/app/components/products-page/products-page.ts
@@ -26,7 +26,7 @@ export class ProductsPage implements OnInit{
   productList: CartItem[] = [];
   filteredList: CartItem[] = [];
   currentPage: number = 1; // תמיד מתחילים מדף ראשון
-  pageSize: number = 5;    
+  pageSize: number = 4;    
 
   currentUserIsAdmin: boolean = false; // שיניתי ל-true כדי שתוכלי לראות שהעריכה עובדת
 
